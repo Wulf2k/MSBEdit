0dce316b8454d61304f9e982d3ea9ba715e31496
Wulf2k <wulf2k@gmail.com>
2016-01-11 16:02:31 -0600

    Start of MSB tool, no useful functionality.

---
+++ MSBEdit/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMSBEdit
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.txtMSBfile = New System.Windows.Forms.TextBox()
+        Me.lblGAFile = New System.Windows.Forms.Label()
+        Me.dgvParams = New System.Windows.Forms.DataGridView()
+        CType(Me.dgvParams, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(489, 41)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 35
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Location = New System.Drawing.Point(411, 41)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpen.TabIndex = 34
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'txtMSBfile
+        '
+        Me.txtMSBfile.AllowDrop = True
+        Me.txtMSBfile.Location = New System.Drawing.Point(46, 14)
+        Me.txtMSBfile.Name = "txtMSBfile"
+        Me.txtMSBfile.Size = New System.Drawing.Size(518, 20)
+        Me.txtMSBfile.TabIndex = 31
+        '
+        'lblGAFile
+        '
+        Me.lblGAFile.AutoSize = True
+        Me.lblGAFile.Location = New System.Drawing.Point(14, 17)
+        Me.lblGAFile.Name = "lblGAFile"
+        Me.lblGAFile.Size = New System.Drawing.Size(26, 13)
+        Me.lblGAFile.TabIndex = 33
+        Me.lblGAFile.Text = "File:"
+        '
+        'dgvParams
+        '
+        Me.dgvParams.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvParams.Location = New System.Drawing.Point(17, 219)
+        Me.dgvParams.Name = "dgvParams"
+        Me.dgvParams.Size = New System.Drawing.Size(595, 510)
+        Me.dgvParams.TabIndex = 37
+        '
+        'frmMSBEdit
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(621, 737)
+        Me.Controls.Add(Me.dgvParams)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.txtMSBfile)
+        Me.Controls.Add(Me.lblGAFile)
+        Me.Name = "frmMSBEdit"
+        Me.Text = "Wulf's MSB Editor"
+        CType(Me.dgvParams, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnOpen As System.Windows.Forms.Button
+    Friend WithEvents txtMSBfile As System.Windows.Forms.TextBox
+    Friend WithEvents lblGAFile As System.Windows.Forms.Label
+    Friend WithEvents dgvParams As System.Windows.Forms.DataGridView
+
+End Class
---
+++ MSBEdit/Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports System.IO
+
+Public Class frmMSBEdit
+
+    Public Shared bytes() As Byte
+    Public Shared bigEndian As Boolean = True
+
+    Private Sub txt_Drop(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtMSBfile.DragDrop
+        Dim file() As String = e.Data.GetData(DataFormats.FileDrop)
+        sender.Text = file(0)
+    End Sub
+    Private Sub txt_DragEnter(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtMSBfile.DragEnter
+        e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Function StrFromBytes(ByVal loc As UInteger) As String
+        Dim Str As String = ""
+        Dim cont As Boolean = True
+
+        While cont
+            If bytes(loc) > 0 Then
+                Str = Str + Convert.ToChar(bytes(loc))
+                loc += 1
+            Else
+                cont = False
+            End If
+        End While
+
+        Return Str
+    End Function
+    Private Function UIntFromBytes(ByVal loc As UInteger) As UInteger
+        Dim tmpUint As UInteger = 0
+
+        If bigEndian Then
+            For i = 0 To 3
+                tmpUint += Convert.ToUInt32(bytes(loc + i)) * &H100 ^ (3 - i)
+            Next
+        Else
+            For i = 0 To 3
+                tmpUint += Convert.ToUInt32(bytes(loc + 3 - i)) * &H100 ^ (3 - i)
+            Next
+        End If
+
+        Return tmpUint
+    End Function
+
+    Private Sub InsBytes(ByVal bytes2() As Byte, ByVal loc As UInteger)
+        Array.Copy(bytes2, 0, bytes, loc, bytes2.Length)
+    End Sub
+    Private Sub StrToBytes(ByVal str As String, ByVal loc As UInteger)
+        Dim BArr() As Byte
+        BArr = System.Text.Encoding.ASCII.GetBytes(str)
+
+        Array.Copy(BArr, 0, bytes, loc, BArr.Length)
+    End Sub
+    Private Sub UINTToBytes(ByVal val As UInteger, loc As UInteger)
+        Dim BArr(3) As Byte
+
+        If bigEndian Then
+            For i = 0 To 3
+                BArr(i) = Math.Floor(val / (&H100 ^ (3 - i))) Mod &H100
+            Next
+        Else
+            For i = 0 To 3
+                BArr(3 - i) = Math.Floor(val / (&H100 ^ (3 - i))) Mod &H100
+            Next
+        End If
+        Array.Copy(BArr, 0, bytes, loc, 4)
+    End Sub
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        bytes = File.ReadAllBytes(txtMSBfile.Text)
+
+        Dim modelPtr As UInteger
+        Dim modelCnt As UInteger
+
+        Dim eventPtr As UInteger
+        Dim eventCnt As UInteger
+
+        Dim pointPtr As UInteger
+        Dim pointCnt As UInteger
+
+        Dim partsPtr As UInteger
+        Dim partsCnt As UInteger
+
+        If UIntFromBytes(&H8) > &H10000 Then bigEndian = False
+
+        modelPtr = UIntFromBytes(&H4)
+        modelCnt = UIntFromBytes(&H8)
+
+
+        eventPtr = UIntFromBytes((modelCnt * &H4) + &H8)
+        eventCnt = UIntFromBytes(eventPtr + &H8)
+
+        pointPtr = UIntFromBytes((eventCnt * &H4) + &H8 + eventPtr)
+        pointCnt = UIntFromBytes(pointPtr + &H8)
+
+        partsPtr = UIntFromBytes((pointCnt * &H4) + &H8 + pointPtr)
+        partsCnt = UIntFromBytes(partsPtr + &H8)
+
+        MsgBox(Hex(partsPtr))
+
+
+
+
+
+    End Sub
+End Class
