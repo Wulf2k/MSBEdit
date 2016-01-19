edf8028575622a558d0ad1b4cda6d18a26626c8e
Wulf2k <wulf2k@gmail.com>
2016-01-19 14:22:48 -0600

    Basic editing support.

--- MSBEdit/Form1.Designer.vb
+++ MSBEdit/Form1.Designer.vb
@@ -26,28 +26,12 @@ Partial Class frmMSBEdit
         Me.btnOpen = New System.Windows.Forms.Button()
         Me.txtMSBfile = New System.Windows.Forms.TextBox()
         Me.lblGAFile = New System.Windows.Forms.Label()
-        Me.dgvModel = New System.Windows.Forms.DataGridView()
+        Me.dgvCreature = New System.Windows.Forms.DataGridView()
         Me.tabParams = New System.Windows.Forms.TabControl()
-        Me.tabModel = New System.Windows.Forms.TabPage()
-        Me.tabEvent = New System.Windows.Forms.TabPage()
-        Me.tabPoint = New System.Windows.Forms.TabPage()
-        Me.tabParts = New System.Windows.Forms.TabPage()
-        Me.tabMapstudio = New System.Windows.Forms.TabPage()
-        Me.dgvEvent = New System.Windows.Forms.DataGridView()
-        Me.dgvPoint = New System.Windows.Forms.DataGridView()
-        Me.dgvParts = New System.Windows.Forms.DataGridView()
-        Me.dgvMapstudio = New System.Windows.Forms.DataGridView()
-        CType(Me.dgvModel, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.tabCreatures = New System.Windows.Forms.TabPage()
+        CType(Me.dgvCreature, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.tabParams.SuspendLayout()
-        Me.tabModel.SuspendLayout()
-        Me.tabEvent.SuspendLayout()
-        Me.tabPoint.SuspendLayout()
-        Me.tabParts.SuspendLayout()
-        Me.tabMapstudio.SuspendLayout()
-        CType(Me.dgvEvent, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.dgvPoint, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.dgvParts, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.dgvMapstudio, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.tabCreatures.SuspendLayout()
         Me.SuspendLayout()
         '
         'btnSave
@@ -85,134 +69,49 @@ Partial Class frmMSBEdit
         Me.lblGAFile.TabIndex = 33
         Me.lblGAFile.Text = "File:"
         '
-        'dgvModel
+        'dgvCreature
         '
-        Me.dgvModel.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvModel.Location = New System.Drawing.Point(6, 6)
-        Me.dgvModel.Name = "dgvModel"
-        Me.dgvModel.Size = New System.Drawing.Size(665, 626)
-        Me.dgvModel.TabIndex = 37
+        Me.dgvCreature.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvCreature.Location = New System.Drawing.Point(6, 6)
+        Me.dgvCreature.Name = "dgvCreature"
+        Me.dgvCreature.Size = New System.Drawing.Size(1107, 626)
+        Me.dgvCreature.TabIndex = 37
         '
         'tabParams
         '
-        Me.tabParams.Controls.Add(Me.tabModel)
-        Me.tabParams.Controls.Add(Me.tabEvent)
-        Me.tabParams.Controls.Add(Me.tabPoint)
-        Me.tabParams.Controls.Add(Me.tabParts)
-        Me.tabParams.Controls.Add(Me.tabMapstudio)
+        Me.tabParams.Controls.Add(Me.tabCreatures)
         Me.tabParams.Location = New System.Drawing.Point(12, 70)
         Me.tabParams.Name = "tabParams"
         Me.tabParams.SelectedIndex = 0
-        Me.tabParams.Size = New System.Drawing.Size(685, 664)
+        Me.tabParams.Size = New System.Drawing.Size(1127, 664)
         Me.tabParams.TabIndex = 38
         '
-        'tabModel
+        'tabCreatures
         '
-        Me.tabModel.Controls.Add(Me.dgvModel)
-        Me.tabModel.Location = New System.Drawing.Point(4, 22)
-        Me.tabModel.Name = "tabModel"
-        Me.tabModel.Padding = New System.Windows.Forms.Padding(3)
-        Me.tabModel.Size = New System.Drawing.Size(677, 638)
-        Me.tabModel.TabIndex = 0
-        Me.tabModel.Text = "MODEL_PARAM_ST"
-        Me.tabModel.UseVisualStyleBackColor = True
-        '
-        'tabEvent
-        '
-        Me.tabEvent.Controls.Add(Me.dgvEvent)
-        Me.tabEvent.Location = New System.Drawing.Point(4, 22)
-        Me.tabEvent.Name = "tabEvent"
-        Me.tabEvent.Padding = New System.Windows.Forms.Padding(3)
-        Me.tabEvent.Size = New System.Drawing.Size(677, 638)
-        Me.tabEvent.TabIndex = 1
-        Me.tabEvent.Text = "EVENT_PARAM_ST"
-        Me.tabEvent.UseVisualStyleBackColor = True
-        '
-        'tabPoint
-        '
-        Me.tabPoint.Controls.Add(Me.dgvPoint)
-        Me.tabPoint.Location = New System.Drawing.Point(4, 22)
-        Me.tabPoint.Name = "tabPoint"
-        Me.tabPoint.Size = New System.Drawing.Size(677, 638)
-        Me.tabPoint.TabIndex = 2
-        Me.tabPoint.Text = "POINT_PARAM_ST"
-        Me.tabPoint.UseVisualStyleBackColor = True
-        '
-        'tabParts
-        '
-        Me.tabParts.Controls.Add(Me.dgvParts)
-        Me.tabParts.Location = New System.Drawing.Point(4, 22)
-        Me.tabParts.Name = "tabParts"
-        Me.tabParts.Size = New System.Drawing.Size(677, 638)
-        Me.tabParts.TabIndex = 3
-        Me.tabParts.Text = "PARTS_PARAM_ST"
-        Me.tabParts.UseVisualStyleBackColor = True
-        '
-        'tabMapstudio
-        '
-        Me.tabMapstudio.Controls.Add(Me.dgvMapstudio)
-        Me.tabMapstudio.Location = New System.Drawing.Point(4, 22)
-        Me.tabMapstudio.Name = "tabMapstudio"
-        Me.tabMapstudio.Size = New System.Drawing.Size(677, 638)
-        Me.tabMapstudio.TabIndex = 4
-        Me.tabMapstudio.Text = "MAPSTUDIO_TREE_ST"
-        Me.tabMapstudio.UseVisualStyleBackColor = True
-        '
-        'dgvEvent
-        '
-        Me.dgvEvent.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvEvent.Location = New System.Drawing.Point(6, 6)
-        Me.dgvEvent.Name = "dgvEvent"
-        Me.dgvEvent.Size = New System.Drawing.Size(665, 626)
-        Me.dgvEvent.TabIndex = 39
-        '
-        'dgvPoint
-        '
-        Me.dgvPoint.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvPoint.Location = New System.Drawing.Point(6, 6)
-        Me.dgvPoint.Name = "dgvPoint"
-        Me.dgvPoint.Size = New System.Drawing.Size(665, 626)
-        Me.dgvPoint.TabIndex = 38
-        '
-        'dgvParts
-        '
-        Me.dgvParts.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvParts.Location = New System.Drawing.Point(6, 6)
-        Me.dgvParts.Name = "dgvParts"
-        Me.dgvParts.Size = New System.Drawing.Size(665, 626)
-        Me.dgvParts.TabIndex = 38
-        '
-        'dgvMapstudio
-        '
-        Me.dgvMapstudio.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.dgvMapstudio.Location = New System.Drawing.Point(6, 6)
-        Me.dgvMapstudio.Name = "dgvMapstudio"
-        Me.dgvMapstudio.Size = New System.Drawing.Size(665, 626)
-        Me.dgvMapstudio.TabIndex = 38
+        Me.tabCreatures.Controls.Add(Me.dgvCreature)
+        Me.tabCreatures.Location = New System.Drawing.Point(4, 22)
+        Me.tabCreatures.Name = "tabCreatures"
+        Me.tabCreatures.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabCreatures.Size = New System.Drawing.Size(1119, 638)
+        Me.tabCreatures.TabIndex = 0
+        Me.tabCreatures.Text = "Creatures"
+        Me.tabCreatures.UseVisualStyleBackColor = True
         '
         'frmMSBEdit
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(706, 746)
+        Me.ClientSize = New System.Drawing.Size(1151, 747)
         Me.Controls.Add(Me.tabParams)
         Me.Controls.Add(Me.btnSave)
         Me.Controls.Add(Me.btnOpen)
         Me.Controls.Add(Me.txtMSBfile)
         Me.Controls.Add(Me.lblGAFile)
         Me.Name = "frmMSBEdit"
-        Me.Text = "Wulf's MSB Editor"
-        CType(Me.dgvModel, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Text = "Wulf's MSB Editor v0.100"
+        CType(Me.dgvCreature, System.ComponentModel.ISupportInitialize).EndInit()
         Me.tabParams.ResumeLayout(False)
-        Me.tabModel.ResumeLayout(False)
-        Me.tabEvent.ResumeLayout(False)
-        Me.tabPoint.ResumeLayout(False)
-        Me.tabParts.ResumeLayout(False)
-        Me.tabMapstudio.ResumeLayout(False)
-        CType(Me.dgvEvent, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.dgvPoint, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.dgvParts, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.dgvMapstudio, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.tabCreatures.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -221,16 +120,8 @@ Partial Class frmMSBEdit
     Friend WithEvents btnOpen As System.Windows.Forms.Button
     Friend WithEvents txtMSBfile As System.Windows.Forms.TextBox
     Friend WithEvents lblGAFile As System.Windows.Forms.Label
-    Friend WithEvents dgvModel As System.Windows.Forms.DataGridView
+    Friend WithEvents dgvCreature As System.Windows.Forms.DataGridView
     Friend WithEvents tabParams As System.Windows.Forms.TabControl
-    Friend WithEvents tabModel As System.Windows.Forms.TabPage
-    Friend WithEvents tabEvent As System.Windows.Forms.TabPage
-    Friend WithEvents tabPoint As System.Windows.Forms.TabPage
-    Friend WithEvents tabParts As System.Windows.Forms.TabPage
-    Friend WithEvents tabMapstudio As System.Windows.Forms.TabPage
-    Friend WithEvents dgvEvent As System.Windows.Forms.DataGridView
-    Friend WithEvents dgvPoint As System.Windows.Forms.DataGridView
-    Friend WithEvents dgvParts As System.Windows.Forms.DataGridView
-    Friend WithEvents dgvMapstudio As System.Windows.Forms.DataGridView
+    Friend WithEvents tabCreatures As System.Windows.Forms.TabPage
 
 End Class
--- MSBEdit/Form1.vb
+++ MSBEdit/Form1.vb
@@ -28,7 +28,105 @@ Public Class frmMSBEdit
 
         Return Str
     End Function
-    Private Function UIntFromBytes(ByVal loc As UInteger) As UInteger
+
+    Private Function Int16ToTwoByte(ByVal val As Integer) As Byte()
+        If bigEndian Then
+            Return ReverseTwoBytes(BitConverter.GetBytes(Convert.ToInt16(val)))
+        Else
+            Return BitConverter.GetBytes(Convert.ToInt16(val))
+        End If
+    End Function
+    Private Function Int32ToFourByte(ByVal val As Integer) As Byte()
+        If bigEndian Then
+            Return ReverseFourBytes(BitConverter.GetBytes(Convert.ToInt32(val)))
+        Else
+            Return BitConverter.GetBytes(Convert.ToInt32(val))
+        End If
+    End Function
+    Private Function UInt16TotwoByte(ByVal val As UInteger) As Byte()
+        If bigEndian Then
+            Return ReverseTwoBytes(BitConverter.GetBytes(Convert.ToUInt16(val)))
+        Else
+            Return BitConverter.GetBytes(Convert.ToUInt16(val))
+        End If
+    End Function
+    Private Function UInt32ToFourByte(ByVal val As UInteger) As Byte()
+        If bigEndian Then
+            Return ReverseFourBytes(BitConverter.GetBytes(Convert.ToUInt32(val)))
+        Else
+            Return BitConverter.GetBytes(Convert.ToUInt32(val))
+        End If
+    End Function
+    Private Function SingleToFourByte(ByVal val As String) As Byte()
+        If IsNumeric(val) Then
+            If bigEndian Then
+                Return ReverseFourBytes(BitConverter.GetBytes(Convert.ToSingle(val)))
+            Else
+                Return BitConverter.GetBytes(Convert.ToSingle(val))
+            End If
+        Else
+            Return {0, 0, 0, 0}
+        End If
+    End Function
+    Private Function ReverseFourBytes(ByVal byt() As Byte)
+        Return {byt(3), byt(2), byt(1), byt(0)}
+    End Function
+    Private Function ReverseTwoBytes(ByVal byt() As Byte)
+        Return {byt(1), byt(0)}
+    End Function
+    Private Sub InsBytes(ByVal loc As UInteger, ByVal byt As Byte())
+        For i = 0 To byt.Length - 1
+            bytes(loc + i) = byt(i)
+        Next
+    End Sub
+
+    Private Function SingleFromFour(ByVal loc As UInteger) As Single
+        Dim bArray(3) As Byte
+
+        For i = 0 To 3
+            bArray(3 - i) = bytes(loc + i)
+        Next
+        If Not bigEndian Then bArray = ReverseFourBytes(bArray)
+        Return BitConverter.ToSingle(bArray, 0)
+    End Function
+    Private Function SIntFromTwo(ByVal loc As UInteger) As Int16
+        Dim tmpint As Integer = 0
+        Dim bArray(1) As Byte
+
+        For i = 0 To 1
+            bArray(1 - i) = bytes(loc + i)
+        Next
+        If Not bigEndian Then bArray = ReverseTwoBytes(bArray)
+        tmpint = BitConverter.ToInt16(bArray, 0)
+        Return tmpint
+    End Function
+    Private Function SIntFromFour(ByVal loc As UInteger) As Integer
+        Dim tmpint As Integer = 0
+        Dim bArray(3) As Byte
+
+        For i = 0 To 3
+            bArray(3 - i) = bytes(loc + i)
+        Next
+        If Not bigEndian Then bArray = ReverseFourBytes(bArray)
+        tmpint = BitConverter.ToInt32(bArray, 0)
+        Return tmpint
+    End Function
+    Private Function UIntFromTwo(ByVal loc As UInteger) As UInteger
+        Dim tmpUint As UInteger = 0
+
+        If bigEndian Then
+            For i = 0 To 1
+                tmpUint += Convert.ToUInt16(bytes(loc + i)) * &H100 ^ (1 - i)
+            Next
+        Else
+            For i = 0 To 1
+                tmpUint += Convert.ToUInt16(bytes(loc + i)) * &H100 ^ (i)
+            Next
+        End If
+
+        Return tmpUint
+    End Function
+    Private Function UIntFromFour(ByVal loc As UInteger) As UInteger
         Dim tmpUint As UInteger = 0
 
         If bigEndian Then
@@ -37,36 +135,12 @@ Public Class frmMSBEdit
             Next
         Else
             For i = 0 To 3
-                tmpUint += Convert.ToUInt32(bytes(loc + 3 - i)) * &H100 ^ (3 - i)
+                tmpUint += Convert.ToUInt32(bytes(loc + i)) * &H100 ^ (i)
             Next
         End If
 
         Return tmpUint
     End Function
-
-    Private Sub InsBytes(ByVal bytes2() As Byte, ByVal loc As UInteger)
-        Array.Copy(bytes2, 0, bytes, loc, bytes2.Length)
-    End Sub
-    Private Sub StrToBytes(ByVal str As String, ByVal loc As UInteger)
-        Dim BArr() As Byte
-        BArr = System.Text.Encoding.ASCII.GetBytes(str)
-
-        Array.Copy(BArr, 0, bytes, loc, BArr.Length)
-    End Sub
-    Private Sub UINTToBytes(ByVal val As UInteger, loc As UInteger)
-        Dim BArr(3) As Byte
-
-        If bigEndian Then
-            For i = 0 To 3
-                BArr(i) = Math.Floor(val / (&H100 ^ (3 - i))) Mod &H100
-            Next
-        Else
-            For i = 0 To 3
-                BArr(3 - i) = Math.Floor(val / (&H100 ^ (3 - i))) Mod &H100
-            Next
-        End If
-        Array.Copy(BArr, 0, bytes, loc, 4)
-    End Sub
 
     Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
         bytes = File.ReadAllBytes(txtMSBfile.Text)
@@ -86,111 +160,229 @@ Public Class frmMSBEdit
         Dim mapstudioPtr As UInteger
         Dim mapstudioCnt As UInteger
 
-
-        If UIntFromBytes(&H8) > &H10000 Then bigEndian = False
-
-        modelPtr = UIntFromBytes(&H4)
-        modelCnt = UIntFromBytes(&H8)
-
-
-        eventPtr = UIntFromBytes((modelCnt * &H4) + &H8)
-        eventCnt = UIntFromBytes(eventPtr + &H8)
-
-        pointPtr = UIntFromBytes((eventCnt * &H4) + &H8 + eventPtr)
-        pointCnt = UIntFromBytes(pointPtr + &H8)
-
-        partsPtr = UIntFromBytes((pointCnt * &H4) + &H8 + pointPtr)
-        partsCnt = UIntFromBytes(partsPtr + &H8)
+        bigEndian = True
+        If UIntFromFour(&H8) > &H10000 Then
+            bigEndian = False
+        End If
 
 
+        modelPtr = UIntFromFour(&H4)
+        modelCnt = UIntFromFour(&H8)
 
 
+        eventPtr = UIntFromFour((modelCnt * &H4) + &H8)
+        eventCnt = UIntFromFour(eventPtr + &H8)
 
-        dgvParts.Rows.Clear()
-        dgvParts.Columns.Clear()
+        pointPtr = UIntFromFour((eventCnt * &H4) + &H8 + eventPtr)
+        pointCnt = UIntFromFour(pointPtr + &H8)
 
-        dgvParts.Columns.Add("Unknown 1", "Unknown 1")
-        dgvParts.Columns.Add("Object type", "Object type")
-        dgvParts.Columns.Add("Type index", "Type index")
-        dgvParts.Columns.Add("Unknown 2", "Unknown 2")
-
-        dgvParts.Columns.Add("Unknown 3", "Unknown 3")
-        dgvParts.Columns.Add("X pos", "X pos")
-        dgvParts.Columns.Add("Y pos", "Y pos")
-        dgvParts.Columns.Add("Z pos", "Z pos")
-
-        dgvParts.Columns.Add("Unknown 4", "Unknown 4")
-        dgvParts.Columns.Add("Facing", "Facing")
-        dgvParts.Columns.Add("Unknown 5", "Unknown 5")
-        dgvParts.Columns.Add("Unknown 6", "Unknown 6")  '1.0
-
-        dgvParts.Columns.Add("Unknown 7", "Unknown 7")  '1.0
-        dgvParts.Columns.Add("Unknown 8", "Unknown 8")  '1.0
-        dgvParts.Columns.Add("Unknown 9", "Unknown 9")  '1 for h
-        dgvParts.Columns.Add("Unknown 10", "Unknown 10")    '0
-
-        dgvParts.Columns.Add("Unknown 11", "Unknown 11")    '0
-        dgvParts.Columns.Add("Unknown 12", "Unknown 12")    '0
-        dgvParts.Columns.Add("Unknown 13", "Unknown 13")    '1 for h
-        dgvParts.Columns.Add("Unknown 14", "Unknown 14")    '0s
-
-        dgvParts.Columns.Add("Unknown 15", "Unknown 15")    '0
-        dgvParts.Columns.Add("Unknown 16", "Unknown 16")    '0
-        dgvParts.Columns.Add("Unknown 17", "Unknown 17")    'something
-        dgvParts.Columns.Add("Unknown 18", "Unknown 18")    'something
-
-        dgvParts.Columns.Add("Unknown 19", "Unknown 19")
-        dgvParts.Columns.Add("Unknown 20", "Unknown 20")
-
-        dgvParts.Columns.Add("Object name", "Object name")
-        dgvParts.Columns.Add("SIB path", "SIB path")
-
-        dgvParts.Columns.Add("ID in scripts", "ID in scripts")
-        dgvParts.Columns.Add("Unknown 23", "Unknown 23")    '0
-        dgvParts.Columns.Add("Unknown 24", "Unknown 24")    '0
-
-        dgvParts.Columns.Add("Unknown 25", "Unknown 25")    '00010100
-
-        dgvParts.Columns.Add("Unknown 26", "Unknown 26")    '0
-        dgvParts.Columns.Add("Unknown 27", "Unknown 27")    '0
-
-        dgvParts.Columns.Add("Unknown 28", "Unknown 28")   '08000000 for h
-
-        dgvParts.Columns.Add("Unknown 29", "Unknown 29")    '0
-        dgvParts.Columns.Add("Unknown 30", "Unknown 30")    '00000001 for h
-
-        dgvParts.Columns.Add("NPC ID", "NPC ID")    '0
-
-        dgvParts.Columns.Add("ID in scripts#2", "ID in scripts#2")    '0
-        dgvParts.Columns.Add("Unknown 33", "Unknown 33")    '0
-        dgvParts.Columns.Add("Unknown 34", "Unknown 34")    '0008ffff for h
-        dgvParts.Columns.Add("Unknown 35", "Unknown 35")
-
-        dgvParts.Columns.Add("Unknown 36", "Unknown 36")
-        dgvParts.Columns.Add("Unknown 37", "Unknown 37")
-        dgvParts.Columns.Add("Unknown 38", "Unknown 38")
-        dgvParts.Columns.Add("Unknown 39", "Unknown 39")
-
-        dgvParts.Columns.Add("Unknown 40", "Unknown 40")
-        dgvParts.Columns.Add("Unknown 41", "Unknown 41")    '-1s for h
-        dgvParts.Columns.Add("Unknown 42", "Unknown 42")
-        dgvParts.Columns.Add("Unknown 43", "Unknown 43")
-
-        dgvParts.Columns.Add("Unknown 44", "Unknown 44")
-        dgvParts.Columns.Add("Unknown 45", "Unknown 45")
-        dgvParts.Columns.Add("Unknown 46", "Unknown 46")    '0s for h
+        partsPtr = UIntFromFour((pointCnt * &H4) + &H8 + pointPtr)
+        partsCnt = UIntFromFour(partsPtr + &H8)
 
 
 
+        Dim ptr As UInteger
+        Dim crtindex As UInteger
+        Dim xpos As Single
+        Dim ypos As Single
+        Dim zpos As Single
+        Dim facing As Single
+
+        Dim model As String
+        Dim sibpath As String
+
+        Dim scriptid1 As Integer
+        Dim scriptid2 As Integer
+        Dim npcid1 As Integer
+        Dim npcid2 As Integer
+
+        Dim offset As UInteger
+
+        Dim padding As UInteger
+
+        Dim row(12) As String
+
+        dgvCreature.Rows.Clear()
+        dgvCreature.Columns.Clear()
+
+        dgvCreature.Columns.Add("Index", "Index")
+
+        dgvCreature.Columns.Add("X pos", "X pos")
+        dgvCreature.Columns.Add("Y pos", "Y pos")
+        dgvCreature.Columns.Add("Z pos", "Z pos")
+
+        dgvCreature.Columns.Add("Facing", "Facing")
+        dgvCreature.Columns.Add("Model", "Model")
+        dgvCreature.Columns.Add("SibPath", "SibPath")
+
+        dgvCreature.Columns.Add("Script ID #1", "Script ID #1")
+        'dgvCreature.Columns.Add("Script ID #2", "Script ID #2")
+
+        dgvCreature.Columns.Add("NPC ID #1", "NPC ID #1")
+        'dgvCreature.Columns.Add("NPC ID #2", "NPC ID #2")
+
+        dgvCreature.Columns.Add("Offset", "Offset")
+
+        For i = 0 To partsCnt - 1
+            padding = 0
+            ptr = UIntFromFour(partsPtr + &HC + i * &H4)
+            Select Case UIntFromFour(ptr + &H4)
+                Case &H2
+
+                    If bigEndian Then padding = &H4
+
+                    crtindex = UIntFromFour(ptr + &H8)
+                    xpos = SingleFromFour(ptr + &H14)
+                    ypos = SingleFromFour(ptr + &H18)
+                    zpos = SingleFromFour(ptr + &H1C)
+
+                    facing = SingleFromFour(ptr + &H24)
+
+                    model = StrFromBytes(ptr + &H64 + padding)
+                    sibpath = StrFromBytes(ptr + &H64 + model.Length + 1 + padding)
+
+                    If sibpath.Length = 0 Then padding += 4
+                    If Not bigEndian Then padding += 4
+
+                    If Not ((sibpath.Length + model.Length + 2) Mod 4) = 0 Then
+                        padding += sibpath.Length + model.Length + 2
+                        padding += (4 - (padding Mod 4))
+                    Else
+                        padding += sibpath.Length + model.Length + 2
+                    End If
+
+                    scriptid1 = SIntFromFour(ptr + &H64 + padding)
+                    npcid1 = SIntFromFour(ptr + &H64 + padding + &H24)
+
+                    offset = ptr
+
+                    row(0) = crtindex
+                    row(1) = xpos
+                    row(2) = ypos
+                    row(3) = zpos
+                    row(4) = facing
+                    row(5) = model
+                    row(6) = sibpath
+                    row(7) = scriptid1
+                    row(8) = npcid1
+
+                    row(9) = offset
+
+                    dgvCreature.Rows.Add(row)
+            End Select
+        Next
+
+        mapstudioPtr = UIntFromFour((partsCnt * &H4) + &H8 + partsPtr)
+        mapstudioCnt = UIntFromFour(mapstudioPtr + &H8)
+
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        bytes = File.ReadAllBytes(txtMSBfile.Text)
+
+        Dim modelPtr As UInteger
+        Dim modelCnt As UInteger
+
+        Dim eventPtr As UInteger
+        Dim eventCnt As UInteger
+
+        Dim pointPtr As UInteger
+        Dim pointCnt As UInteger
+
+        Dim partsPtr As UInteger
+        Dim partsCnt As UInteger
+
+        Dim mapstudioPtr As UInteger
+        Dim mapstudioCnt As UInteger
+
+        bigEndian = True
+        If UIntFromFour(&H8) > &H10000 Then
+            bigEndian = False
+        End If
 
 
-        mapstudioPtr = UIntFromBytes((partsCnt * &H4) + &H8 + partsPtr)
-        mapstudioCnt = UIntFromBytes(mapstudioPtr + &H8)
+        modelPtr = UIntFromFour(&H4)
+        modelCnt = UIntFromFour(&H8)
+
+
+        eventPtr = UIntFromFour((modelCnt * &H4) + &H8)
+        eventCnt = UIntFromFour(eventPtr + &H8)
+
+        pointPtr = UIntFromFour((eventCnt * &H4) + &H8 + eventPtr)
+        pointCnt = UIntFromFour(pointPtr + &H8)
+
+        partsPtr = UIntFromFour((pointCnt * &H4) + &H8 + pointPtr)
+        partsCnt = UIntFromFour(partsPtr + &H8)
 
 
 
+        Dim ptr As UInteger
+        Dim crtindex As UInteger
+        Dim xpos As Single
+        Dim ypos As Single
+        Dim zpos As Single
+        Dim facing As Single
+
+        Dim model As String
+        Dim sibpath As String
+
+        Dim scriptid1 As Integer
+        Dim scriptid2 As Integer
+        Dim npcid1 As Integer
+        Dim npcid2 As Integer
+
+        Dim offset As UInteger
+
+        Dim padding As UInteger
+
+        For i = 0 To dgvCreature.Rows.Count - 2
+            padding = 0
+            ptr = UIntFromFour(partsPtr + &HC + i * &H4)
+
+            If bigEndian Then padding = &H4
+
+            crtindex = dgvCreature.Rows(i).Cells(0).Value
+
+            xpos = dgvCreature.Rows(i).Cells(1).Value
+            ypos = dgvCreature.Rows(i).Cells(2).Value
+            zpos = dgvCreature.Rows(i).Cells(3).Value
+
+            facing = dgvCreature.Rows(i).Cells(4).Value
+
+            model = dgvCreature.Rows(i).Cells(5).Value
+            sibpath = dgvCreature.Rows(i).Cells(6).Value
+
+            scriptid1 = dgvCreature.Rows(i).Cells(7).Value
+            npcid1 = dgvCreature.Rows(i).Cells(8).Value
+            ptr = dgvCreature.Rows(i).Cells(9).Value
+
+            InsBytes(ptr + &H8, UInt32ToFourByte(crtindex))
+            InsBytes(ptr + &H14, SingleToFourByte(xpos))
+            InsBytes(ptr + &H18, SingleToFourByte(ypos))
+            InsBytes(ptr + &H1C, SingleToFourByte(zpos))
+            InsBytes(ptr + &H24, SingleToFourByte(facing))
+
+            InsBytes(ptr + &H64 + padding, System.Text.Encoding.ASCII.GetBytes(model))
+            InsBytes(ptr + &H64 + padding + model.Length + 1, System.Text.Encoding.ASCII.GetBytes(sibpath))
+
+            If sibpath.Length = 0 Then padding += 4
+            If Not bigEndian Then padding += 4
+
+            If Not ((sibpath.Length + model.Length + 2) Mod 4) = 0 Then
+                padding += sibpath.Length + model.Length + 2
+                padding += (4 - (padding Mod 4))
+            Else
+                padding += sibpath.Length + model.Length + 2
+            End If
+
+            InsBytes(ptr + &H64 + padding, Int32ToFourByte(scriptid1))
+            InsBytes(ptr + &H64 + padding + &H24, Int32ToFourByte(npcid1))
+        Next
 
 
 
+        File.WriteAllBytes(txtMSBfile.Text, bytes)
+
+        MsgBox("Save Complete.")
     End Sub
 End Class
