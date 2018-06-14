9cff09bffb4b11366c558f435bf38d2f8fb7a681
Meowmaritus <Meowmaritus@gmail.com>
2018-06-14 11:49:52 -0500

    Made Application Much Less Bad

---
+++ MSBEdit/Program.vb
@@ -0,0 +1,10 @@
+﻿Module Program
+    Public Sub Main(ByVal args() As String)
+        Application.EnableVisualStyles()
+        Dim mainForm As New frmMSBEdit
+        If args.Length > 0 Then
+            frmMSBEdit.AutoOpenMsbFile = args(0)
+        End If
+        Application.Run(mainForm)
+    End Sub
+End Module
